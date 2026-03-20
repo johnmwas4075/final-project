c4af003be85e480843e6edb8142e8345d9772c7b
mwangi2 <johnmwangimaina469@gmail.com>
2026-03-20 14:24:28 +0300

    primpt

---
+++ prompt.js
@@ -0,0 +1,10 @@
+now to create the notification part...let guests be notified. on clicking the notification Icon, it should open the notification page with all the notifications. there 
+when a person is to checkin at least a day before, and the time the check in is to happen
+when payment from clients go to host
+when a client rates or makes a comment to one of the properties after visiting
+
+
+and the client to be notified when money is deducted to pay the host
+day before check in and check in day and check out day and notification to rate and comment the property
+
+the notification icon to be responsive i.e if there are three notifications, number 3 should show on the Icon and until the host/client clicks each notification, the number stays like that
